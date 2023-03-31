69b6c928fa7c47da3f5647e3a851aec32736955e
Wudthedud <wud@middleton.school.nz>
2023-03-31 14:58:30 +1300

    update

--- Lucky Unicorn V1.py
+++ Lucky Unicorn V1.py
@@ -3,7 +3,7 @@ Lucky Unicorn Version 1
 Yes/No Checker
 By Daniel Wu
 """
-global question
+
 # Add constants
 ZEBRA_TOKEN = 0.5
 HORSE_TOKEN = 0.5
@@ -12,25 +12,20 @@ DONKEY_TOKEN = 0
 
 
 # Yes/No Checker
-def yes_no():
-    answer = input(question)
-    if answer.upper() == "YES" or answer.upper() == "Y":
-        return True
-    elif answer.upper() == "NO" or answer.upper() == "N":
-        return False
-    else:
-        print("Please enter (Yes/No)")
-        return None
+def yes_no(question):
+    while True:
+        answer = input(question).strip().lower()
+        if answer in ["yes", "y"]:
+            return True
+        elif answer in ["no", "n"]:
+            return False
+        else:
+            print("Please enter (Yes/No)")
 
 
-# Ask user if they have played before: if yes, program continue, if no, show instruction screen
+# Ask user if they have played before: if yes, program continues, if no, show instruction screen
 question = "Have you played before? \n"
-if yes_no():
+if yes_no(question):
     print("Continue")
-elif not yes_no():
-    print("Show instructions")
 else:
-    print("Error")
-    yes_no()
-
-# Error checking
+    print("Show instructions")
--- Lucky Unicorn V2.py
+++ Lucky Unicorn V2.py
@@ -1,8 +1,9 @@
 """
-Lucky Unicorn Version 1
-Yes/No Checker
+Lucky Unicorn Version 2
+Token Generate and Instructions
 By Daniel Wu
 """
+import random
 
 # Add constants
 ZEBRA_TOKEN = 0.5
@@ -10,20 +11,41 @@ HORSE_TOKEN = 0.5
 UNICORN_TOKEN = 5
 DONKEY_TOKEN = 0
 
+# Token Generator
+def generate():
+    tokens = [["U", 5], ["Z", 0.5], ["H", 0.5], ["D", 0]]
+    number = random.randint(1, 4)
+    return tokens[number]
 
 # Yes/No Checker
-def instructions():
-    # Ask user if they have played before
-    played_before = input("Have you played before? (Y/N) \n")
+def yes_no(question_):
+    while True:
+        answer = input(question_).strip().lower()
+        if answer in ["yes", "y"]:
+            return True
+        elif answer in ["no", "n"]:
+            return False
+        else:
+            print("Please enter (Yes/No)")
 
-    # If yes, program continues
-    if played_before.upper() == "YES" or played_before.upper() == "Y":
+#def payment(amount):
+#    amount *
+
+
+# Ask user if they have played before: if yes, program continues, if no, show instruction screen
+question = "Have you played before? \n"
+if yes_no(question):
+    generate()
+else:
+    print("============ Welcome to Lucky Unicorn ============= \n"
+          "\t Payout Rewards: \n"
+          "\t Unicorn: 3x Payment \n"
+          "\t Zebra: 1/2 of Payment \n"
+          "\t Horse: 1/2 of Payment \n"
+          "\t Donkey: Nothing \n"
+          "===================================================")
+    question = "Do you want to continue? \n"
+    if yes_no(question):
         print("Continue")
-    # If no, show instruction screen
-    elif played_before.upper() == "NO" or played_before.upper() == "N":
-        print("Show instructions")
-
-    # Error checking
     else:
-        print("Please enter (Y/N)")
-
+        print("Exit")
